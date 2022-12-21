32bccc27e94f4857587c88ddc96bf43bf77254a8
f_javier <f_javier@DAW2ALU09>
2022-12-21 12:48:52 +0100

    3.4_2.0

---
+++ src/main/java/org/iesvdm/modelo/Pedido.java
@@ -0,0 +1,19 @@
+package org.iesvdm.modelo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+
+@AllArgsConstructor
+public class Pedido {
+	private int id;
+	private double total;
+	private Date fecha;
+	private int idCliente;
+	private int idComaercial;
+	
+
+}
---
+++ src/main/java/org/iesvdm/modelo/PedidoDTO.java
@@ -0,0 +1,23 @@
+package org.iesvdm.modelo;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+
+public class PedidoDTO extends Pedido {
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	
+	public PedidoDTO(int id, double total, Date fecha, int idCliente, int idComaercial, String nombre, String apellido1,
+			String apellido2) {
+		super(id, total, fecha, idCliente, idComaercial);
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+	}
+
+
+}
